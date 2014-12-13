569a66f44359d31644e3d48b6490b7d794aa90dd
Timothy Binney <tjbinney@fullsail.edu>
2014-12-13 12:24:14 -0500

    Second Commit
    Fixed iOS permission and Ui elements

--- Resources/geo.js
+++ Resources/geo.js
@@ -1,17 +1,22 @@
 var api = require("api");
 
-exports.getPosition = function(){
+exports.getPosition = function() {
 	Ti.Geolocation.purpose = "Application requires coordinates to run";
-	if(Ti.Platform.osname === "iphone"){
-	Ti.Geolocation.getCurrentPosition(function(e){
-		console.log(e.source);
+	if (Ti.Platform.osname === "iphone" || Ti.Platform.osname === "ipad") {
+		Ti.Geolocation.getCurrentPosition(function(e) {
+			console.log(JSON.stringify(e.source));
+			var latitude = e.coords.latitude;
+			var longitude = e.coords.longitude;
+		}, function() {
+			alert("Geo Error!");
+		}, {
+			timeout : 10000
+		});
+	} else {
 		var latitude = 40.7127;
 		var longitude = 74.0059;
-	});
-	}else{
-		var latitude = 40.7127;
-		var longitude = 74.0059;
-};
-	api.getInfo(latitude,longitude);
+	};
+	console.log("the latitude is " + latitude);
+	api.getInfo(latitude, longitude);
 };
 
--- Resources/ui.js
+++ Resources/ui.js
@@ -2,57 +2,57 @@ exports.createUI = function(tblData){
 	console.log(tblData);
 	var win = Ti.UI.createWindow({
 		backgroundColor: "grey",
-		layout: "vertical",
 	});
 	
 	var view = Ti.UI.createView({
 		backgroundColor: "white",
 		height: Ti.UI.SIZE.FILL,
 		width: Ti.UI.SIZE.FILL, 
+		layout: "vertical",
 	});
 	var imageView = Ti.UI.createImageView({
-		top: 50,
+		top: 15,
 		align: "center",
 		height: 150,
 		width: 150,
-		image: icon,
+		image: tblData[0].icon,
 	});
 	
 	var label = Ti.UI.createLabel({
 		bottom: 1,
 		height: 5,
 		font: {fontSize:14},
-		text: obs,
+		text: tblData[0].obs,
 	});
 	
 	var tempLabel = Ti.UI.createLabel({
-		top: 210,
+		top: 20,
 		height: 100,
 		width: 150,
 		font: {fontSize: 26},
 		align: "center",
-		text: temp,
+		text: tblData[0].temp,
 	});
 	
 	var weatherLabel = Ti.UI.createLabel({
-		top: 315,
+		top: 30,
 		height: 5,
 		font: {fontSize: 20},
-		text: weather,
+		text: tblData[0].weather,
 	});
 	var windLabel = Ti.UI.createLabel({
-		top: 335,
+		top: 30,
 		height: 5,
 		align: "center",
 		font: {fontSize: 20},
-		text: "Winds " + wind,
+		text: "Winds " + tblData[0].wind,
 	});
 		
-		view.add(windLabel);
-		view.add(weatherLabel);
-		view.add(label);
 		view.add(tempLabel);
 		view.add(imageView);
+		view.add(label);
+		view.add(windLabel);
+		view.add(weatherLabel);
 		win.add(view);
 		win.open();
 	
